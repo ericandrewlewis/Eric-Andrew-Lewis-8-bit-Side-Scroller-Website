a9c1ddedaad7ffdb54140b3cf172f08b2b5ef083
Eric Lewis <eric.andrew.lewis@gmail.com>
2018-02-28 18:11:46 -0500

    Make stars of various sizes and in separate planes

--- version-2/index.js
+++ version-2/index.js
@@ -2,6 +2,16 @@ const randomID = () => {
   return Math.ceil(Math.random * 10000);
 };
 
+const loop = (times, fn) => {
+  new Array(times).fill(0).forEach(fn);
+};
+
+const colors = ["#ef798a", "#f7a9a8", "#613f75", "#e5c3d1", "#988b8e"];
+
+const randomColor = () => {
+  return colors[random(0, 5)];
+};
+
 let draw;
 
 const createSVG = () => {
@@ -15,25 +25,60 @@ const screenWidth = () => {
   return window.innerWidth;
 };
 
+const screenHeight = () => {
+  return window.innerHeight;
+};
+
 const random = (min, max) => {
   const distance = max - min;
   return Math.floor(Math.random() * distance) + min;
 };
 
-const createBlock = () => {
-  const size = random(1, 6);
-  const rect = draw
+// Create a star rectancle.
+const createStarElement = ({ size, x, y, fill }) => {
+  return draw
     .rect(size, size)
-    .attr("x", random(0, screenWidth()))
-    .addClass("rectangle");
+    .addClass("star")
+    .attr("x", x)
+    .attr("y", y)
+    .fill(fill);
+};
+
+// Create a star and a duplicate star for use in animation.
+const createStar = ({ group, size }) => {
+  const x = random(0, screenWidth());
+  const y = random(0, screenHeight());
+  const fill = randomColor();
+  group.add(createStarElement({ x, y, size, fill }));
+  group.add(createStarElement({ x, y: y - screenHeight(), size, fill }));
+};
+
+// Create a group with a povided classname with a bunch of stars.
+const createStars = ({ count, size, className }) => {
+  const group = draw.group().addClass(className);
+  loop(count, () => {
+    createStar({ group, size });
+  });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   createSVG();
-  const numberOfSquares = 50;
-  new Array(numberOfSquares).fill(0).forEach(noop => {
-    const randomDelay = random(0, 4000);
-    setTimeout(createBlock, randomDelay);
+  createStars({
+    count: 100,
+    size: 1,
+    className: "far-stars-container"
+  });
+
+  createStars({
+    count: 50,
+    size: 2,
+    className: "medium-stars-container"
+  });
+
+  createStars({
+    count: 10,
+    size: 3,
+    className: "near-stars-container"
   });
 });
 
